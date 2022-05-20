f30b6cda9174bd1938be7345d90ac62cbc3d272f
tkravits <taylor.kravits@gmail.com>
2022-05-20 17:30:42 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# House of Lines Solution
+
+## Purpose
+To determine given a graph which contains a valid Euler path, determine how many Euler paths exists
+
+## How to Use
+This will generate a static website using nodeJS. Clone the repo to your local machine and run `node server.js` and the site will pop up on `localhost:5000` containing a link that will ask you the number of even nodes and odd nodes. This will return the number of valid paths, the time it takes to calculate, and the Big O notation for calculating this
+
+## Project Maintainer
+Taylor Kravits (taylor.kravits@gmail.com)
---
+++ server.js
@@ -0,0 +1,27 @@
+// var http = require('http'),
+//     fs = require('fs');
+
+
+// fs.readFile('house_of_lines.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(8000);
+// });
+// Declare static folder to be served. It contains the JavaScript code, images, CSS, etc.
+var express = require('express')
+var app = express()
+
+//setting middleware
+app.use(express.static('public')); //Serves resources from public folder
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/solution1.html');
+})
+
+var server = app.listen(3000);
