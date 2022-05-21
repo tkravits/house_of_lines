ca30efa021fc460fcd8df049562e44cbd53f1b8f
tkravits <taylor.kravits@gmail.com>
2022-05-21 11:12:20 -0600

    chore: code cleanup

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-// var http = require('http'),
-//     fs = require('fs');
-
-
-// fs.readFile('house_of_lines.html', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     http.createServer(function(request, response) {  
-//         response.writeHeader(200, {"Content-Type": "text/html"});  
-//         response.write(html);  
-//         response.end();  
-//     }).listen(8000);
-// });
 // Declare static folder to be served. It contains the JavaScript code, images, CSS, etc.
 import express from 'express';
 var app = express()
@@ -23,7 +9,6 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index.html');
     res.sendFile(__dirname + '/public/fleury.html');
     res.sendFile(__dirname + '/public/hierholzer.html');
-    res.sendFile(__dirname + '/public/euler.html');
 })
 
 var server = app.listen(3000);
