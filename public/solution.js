0232d8df160dea7c8fc267db0715ef9d2550abfc
tkravits <taylor.kravits@gmail.com>
2022-05-21 10:08:24 -0600

    feat: adding JS code from https://github.com/trekhleb/javascript-algorithms/

--- public/solution.js
+++ public/solution.js
@@ -1,1 +1,48 @@
-console.log('Hello World');
+import GraphVertex from './graph/GraphVertex.js';
+import GraphEdge from './graph/GraphEdge.js';
+import Graph from './graph/Graph.js';
+import eulerianPath from './eulerian-path/eulerianPath.js';
+
+
+function findEulerianPathInNotEulerianGraph() {
+
+    const vertexA = new GraphVertex('A');
+    const vertexB = new GraphVertex('B');
+    const vertexC = new GraphVertex('C');
+    const vertexD = new GraphVertex('D');
+    const vertexE = new GraphVertex('E');
+    const vertexF = new GraphVertex('F');
+    const vertexG = new GraphVertex('G');
+
+    const edgeAB = new GraphEdge(vertexA, vertexB);
+    const edgeAE = new GraphEdge(vertexA, vertexE);
+    const edgeAF = new GraphEdge(vertexA, vertexF);
+    const edgeAG = new GraphEdge(vertexA, vertexG);
+    const edgeGF = new GraphEdge(vertexG, vertexF);
+    const edgeBE = new GraphEdge(vertexB, vertexE);
+    const edgeEB = new GraphEdge(vertexE, vertexB);
+    const edgeBC = new GraphEdge(vertexB, vertexC);
+    const edgeED = new GraphEdge(vertexE, vertexD);
+    const edgeCD = new GraphEdge(vertexC, vertexD);
+
+    const graph = new Graph();
+
+    graph
+      .addEdge(edgeAB)
+      .addEdge(edgeAE)
+      .addEdge(edgeAF)
+      .addEdge(edgeAG)
+      .addEdge(edgeGF)
+      .addEdge(edgeBE)
+      .addEdge(edgeEB)
+      .addEdge(edgeBC)
+      .addEdge(edgeED)
+      .addEdge(edgeCD);
+
+    const graphEdgesCount = graph.getAllEdges().length;
+
+    const eulerianPathSet = eulerianPath(graph);
+    console.log(eulerianPathSet);
+  };
+
+findEulerianPathInNotEulerianGraph();
