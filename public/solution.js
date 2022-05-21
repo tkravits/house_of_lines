e738b8dd826e70d43bb8fc41e55ff9cbf5dcf040
tkravits <taylor.kravits@gmail.com>
2022-05-21 10:52:04 -0600

    feat: working solution for 1st problem

--- public/solution.js
+++ public/solution.js
@@ -4,7 +4,46 @@ import Graph from './graph/Graph.js';
 import eulerianPath from './eulerian-path/eulerianPath.js';
 
 
-function findEulerianPathInNotEulerianGraph() {
+function findEulerianPathFirst() {
+    const start = performance.now();
+    const vertexA = new GraphVertex('A');
+    const vertexB = new GraphVertex('B');
+    const vertexC = new GraphVertex('C');
+    const vertexD = new GraphVertex('D');
+    const vertexE = new GraphVertex('E');
+
+
+    const edgeAB = new GraphEdge(vertexA, vertexB);
+    const edgeBD = new GraphEdge(vertexB, vertexD);
+    const edgeDE= new GraphEdge(vertexD, vertexE);
+    const edgeEC = new GraphEdge(vertexE, vertexC);
+    const edgeCA = new GraphEdge(vertexC, vertexA);
+    const edgeAD = new GraphEdge(vertexA, vertexD);
+    const edgeDC = new GraphEdge(vertexD, vertexC);
+    const edgeCB = new GraphEdge(vertexC, vertexB);
+
+
+    const graph = new Graph();
+
+    graph
+      .addEdge(edgeAB)
+      .addEdge(edgeBD)
+      .addEdge(edgeDE)
+      .addEdge(edgeEC)
+      .addEdge(edgeCA)
+      .addEdge(edgeAD)
+      .addEdge(edgeDC)
+      .addEdge(edgeCB)
+
+    const graphEdgesCount = graph.getAllEdges().length;
+
+    const eulerianPathSet = eulerianPath(graph);
+    const duration = performance.now() - start;
+    console.log(`Duration: ${duration}ms`);
+    return [eulerianPathSet, duration];
+  };
+
+  function findEulerianPathSecond() {
     const start = performance.now();
     const vertexA = new GraphVertex('A');
     const vertexB = new GraphVertex('B');
@@ -46,11 +85,13 @@ function findEulerianPathInNotEulerianGraph() {
     return [eulerianPathSet, duration];
   };
 
-
 let firstSolution = document.getElementById("firstSolution")
 let timeFirstSolution = document.getElementById("timeFirstSolution")
+let timeSecondSolution = document.getElementById("timeSecondSolution")
 let secondSolution = document.getElementById("secondSolution")
-const [firstResult, timeResult] = findEulerianPathInNotEulerianGraph();
+const [firstResult, timeResult] = findEulerianPathFirst();
+const [secondResult, timeResult2] = findEulerianPathSecond();
 firstSolution.textContent = `${firstResult}`;
 timeFirstSolution.textContent = `Time: ${timeResult.toFixed(2)} ms`;
-console.log(timeResult);
+secondSolution.textContent = `${secondResult}`;
+timeSecondSolution.textContent = `Time: ${timeResult2.toFixed(2)} ms`;
